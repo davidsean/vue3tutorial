392cc40594573955e202fd589b827e73557d2670
David Sean <david.sean@gmail.com>
2021-03-27 16:51:01 -0400

    end chapt2

---
+++ vueschool-forum/.eslintrc.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+
+  rules: {
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
---
+++ vueschool-forum/src/components/PageHome.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      THIS IS PAGE HOME
+      <div v-for="thread in threads" v-bind:key="thread.id">
+        <h2> {{ thread.title }} </h2>
+        <div v-for="postId in thread.posts" v-bind:key="postId">
+          <p><b> {{ userById(postById(postId).userId).name }} </b></p>
+          <p> {{ postById(postId).text }} </p>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    userById (userId) {
+      return this.users.find(u => u.id === userId)
+    },
+    postById (userId) {
+      return this.posts.find(p => p.id === userId)
+    }
+  }
+
+}
+
+</script>
+
+<style>
+
+</style>
